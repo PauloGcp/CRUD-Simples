201ae37c0f21dd55964053c80301246326112a7b
PauloGcp <pauloguilherme.c@hotmail.com>
2022-09-29 12:30:25 -0300

    add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 const todosRoutes = require("./routes")
 const bodyParser = require("body-parser")
 const express = require("express")
+const cors = require("cors")
 
 const app = express()
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 //app.use(express.json())
 app.use(todosRoutes)
 app.get("/health", (req, res)=>{
